7f553ec7e465a115e5d3317c9f9263b3078cec15
Sai Vikas Gopal <saivikas7875@gmail.com>
2019-05-30 02:58:09 -0400

    fetch dog breeds and display in a dropdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import appStyles from './app.module.css';
+import { Container } from 'semantic-ui-react';
+import {SearchBreeds} from './search-breeds';
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={appStyles.app}>
+      <header className={appStyles.appHeader}>
+        <Container>
+          <h2>Know your dog breeds</h2>
+        </Container>
+        <Container>
+          <SearchBreeds/>
+        </Container>
       </header>
     </div>
   );
